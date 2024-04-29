de13bac4f856d6bc1b51e4be88654ea9af5c9e39
gerimundia <https://github.com/gerimundia>
2024-04-28 21:46:05 -0600

    Add first try of data documentation

---
+++ data/data.R
@@ -0,0 +1,15 @@
+#' Datos de la Comisi'on de la verdad de Ecuador
+#'
+#' Datos completos del reporte de la comisi'on de la verdad
+#' 
+#'
+#' @format ## `who`
+#' A data frame with 7,240 rows and 60 columns:
+#' \describe{
+#'   \item{country}{Country name}
+#'   \item{iso2, iso3}{2 & 3 letter ISO country codes}
+#'   \item{year}{Year}
+#'   ...
+#' }
+#' @source <https://www.who.int/teams/global-tuberculosis-programme/data>
+"who"
